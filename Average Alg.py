1e80829268dcd10ebdc327c906554eeff0af72dc
Ajohnl1 <arthur.j.levitan@hotmail.com>
2023-10-09 17:13:17 -0400

    Algorithm exercise

---
+++ Average Alg.py
@@ -0,0 +1,6 @@
+# initial max = number
+# loop x times (based on number of inputs in array)
+#     get number from user input
+#     set sum += input
+# set average = sum / number of inputs from user
+# output average
